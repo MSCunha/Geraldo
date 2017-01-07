14fd3b48fbc92562ff8f18a8d8fa736ad880da45
MSCunha <matheusscunha9@gmail.com>
2017-01-07 08:51:03 -0300

    token

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 
-bot.login('MjY2NDcwODYzMTE5OTc0NDAw.C0-dRg.zhiN30HKI0BM6R5dAUarS6_1iOg');
+bot.login('MjY2NDcwODYzMTE5OTc0NDAw.C1Jn3g.-JOcKWvHT81NeyAgEgJgFtUdZ-4');
 
 bot.on('message', (message) => {
     if(message.content == 'marco'){
