2041d97038e3fe279d3a3c7014f6c73214b819c7
MSCunha <matheusscunha9@gmail.com>
2017-01-07 09:25:04 -0300

    comandos previos

--- index.js
+++ index.js
@@ -5,6 +5,16 @@ bot.login('MjY2NDcwODYzMTE5OTc0NDAw.C1Jn3g.-JOcKWvHT81NeyAgEgJgFtUdZ-4');
 
 bot.on('message', (message) => {
     if(message.content == 'marco'){
-        message.channel.sendMessage('polo');
+        message.channel.sendMessage('polo!');
+    }
+    if(message.content == 'geraldo, o que você faz?'){
+        message.channel.sendMessage('/tts Nada, ainda estou sendo criado :)');
+    }
+    if(message.content == 'Porque você fica offline?'){
+        message.channel.sendMessage('Eu tambem preciso dormir \n');
+        message.channel.sendMessage('--mentira, o servidor é gratuito e tem suas limitações-- "sshiii"');
+    }
+    if(message.content == '!comandos'){
+        message.channel.sendMessage('marco\ngeraldo, o que você faz?\nPorque você fica offline\n')
     }
 });
