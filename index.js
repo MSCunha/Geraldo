a510f104a90fb0fa1249629603029a272f6bf3d8
MSCunha <matheusscunha9@gmail.com>
2017-01-07 22:43:30 -0300

    teste vem pra pedra

--- index.js
+++ index.js
@@ -17,8 +17,8 @@ bot.on('message', (message) => {
     if(message.content == '!comandos'){
         message.channel.sendMessage('1- marco?\n2- qvcfaz?\n3- pqvcficaoffline?\n')
     }
-    if(message.content == '!S2'){
-        message.channel.react(':nightqualira:')
+    if(message.content == '!geraldo vem pra pedra!'){
+        message.channel.sendFile('https://media.giphy.com/media/3o7TKEJuETmXR6f9fO/giphy.gif')
     }
     if(message.content == '!geraldo sai da pedra!'){
         message.channel.sendFile('https://68.media.tumblr.com/1ab432141e6ecebe6f736622ceb761b5/tumblr_oa4cuwZbV71ukxlx4o1_500.gif')
