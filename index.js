5ad442a3641ac44982bc0926e70eb98dabd07b68
MSCunha <matheusscunha9@gmail.com>
2017-01-07 22:47:18 -0300

    teste 886465561151565 pedra

--- index.js
+++ index.js
@@ -17,10 +17,10 @@ bot.on('message', (message) => {
     if(message.content == '!comandos'){
         message.channel.sendMessage('1- marco?\n2- qvcfaz?\n3- pqvcficaoffline?\n')
     }
-    if(message.content == '!geraldo vem pra pedra!'){
+    if(message.content == '!vemprapedra'){
         message.channel.sendFile('https://media.giphy.com/media/3o7TKEJuETmXR6f9fO/giphy.gif')
     }
-    if(message.content == '!geraldo sai da pedra!'){
+    if(message.content == '!saidapedra'){
         message.channel.sendFile('https://68.media.tumblr.com/1ab432141e6ecebe6f736622ceb761b5/tumblr_oa4cuwZbV71ukxlx4o1_500.gif')
     }
 });    
